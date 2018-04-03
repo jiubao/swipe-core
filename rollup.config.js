5c2b7be2e7b4e2e4f5ee827f2b1cd94902085f15
xiadong8 <xiadong8@wanda.cn>
2018-04-03 16:43:49 +0800

    init2

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+// import vue from 'rollup-plugin-vue'
+// import json from 'rollup-plugin-json'
+// import buble from 'rollup-plugin-buble'
+// import commonjs from 'rollup-plugin-commonjs'
+// import nodeResolve from 'rollup-plugin-node-resolve'
+import cfg from './package.json'
+
+export default {
+  input: 'src/index.js',
+  output: [{
+    file: cfg.browser,
+    format: 'umd',
+    name: 'vue-vswiper'
+  }, {
+    file: cfg.module,
+    format: 'es'
+  }, {
+    file: cfg.cjs,
+    format: 'cjs'
+  }]
+}
