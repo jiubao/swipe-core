c5ea277684f31f414ae845bd0cef39e1c134e5b0
jiubao <xia.ldj@gmail.com>
2018-04-11 12:08:19 +0800

    gh pages

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var screenWidth = window.screen.width
 window['swipe-core']({
   root: root1,
   elms: Array.prototype.slice.apply(root1.children[0].children),
-  height: 200
+  height: 200,
   // width: window.screen.width
   // cycle: false,
   // index: 0
@@ -14,7 +14,20 @@ window['swipe-core']({
   elms: document.querySelectorAll('.swiper-item2'),
   width: 315,
   expose: true
-  // width: window.screen.width
-  // cycle: false,
-  // index: 0
+})
+
+var root3 = document.getElementById('swiper3');
+window['swipe-core']({
+  root: root3,
+  elms: Array.prototype.slice.apply(root3.children[0].children),
+  width: 315,
+  height: 230,
+  expose: true
+})
+
+var root4 = document.getElementById('swiper4')
+window['swipe-core']({
+  root: root4,
+  elms: Array.prototype.slice.apply(root4.children[0].children),
+  height: 200
 })
