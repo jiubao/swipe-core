bce4b84b6475b42ca06ef10158cc718e26d2bd22
jiubao <xia.ldj@gmail.com>
2018-04-12 19:45:10 +0800

    click bug

--- swipe-core.umd.js
+++ swipe-core.umd.js
@@ -79,6 +79,7 @@
   var FAST_THRESHOLD = 120;
   var FAST_INTERVAL = 250;
   var MAX_INTERVAL = 1000;
+  var MAX_PART = MAX_INTERVAL * 2 / 3;
   var AUTO_TIMEOUT = 3000;
 
   var defaultOptions = {
@@ -88,8 +89,8 @@
     root: null, // required
     elms: [], // required
     index: 0,
-    width: window.screen.width, // required
-    height: 200, // required
+    width: window.screen.width, // if css is false, need width & height
+    height: 200,
     css: false
   };
 
@@ -207,11 +208,11 @@
     }
 
     function autoCallback () {
-      animations.timeouts.push(setTimeout(function () { return animate(main, x, x - width, MAX_INTERVAL, onAutoAnimation, autoCallback); }, AUTO_TIMEOUT));
+      animations.timeouts.push(setTimeout(function () { return animate(main, x, x - width, MAX_PART, onAutoAnimation, autoCallback); }, AUTO_TIMEOUT));
     }
 
     function onTouchEnd (evt) {
-      if (phase === 4 || currentX === startX) { return }
+      if (phase === 4) { return }
       phase = 2;
       var right = currentX > restartX;
       var fast = (Date.now() - startTime) < FAST_THRESHOLD;
@@ -227,7 +228,7 @@
 
       var to = current.x * -1;
 
-      var t = Math.min(Math.max(MAX_INTERVAL * Math.abs(to - x) / width, FAST_INTERVAL), MAX_INTERVAL * 2 / 3);
+      var t = Math.min(Math.max(MAX_INTERVAL * Math.abs(to - x) / width, FAST_INTERVAL), MAX_PART);
       animate(main, x, to, fast ? FAST_INTERVAL : t);
       auto && autoCallback();
     }
