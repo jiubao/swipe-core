c5f796c048cd12f1dd67153d051f7ef08354067f
jiubao <xia.ldj@gmail.com>
2018-12-07 17:46:42 +0800

    rm utils

--- src/utils.js
+++ src/utils.js
@@ -1,25 +1,3 @@
-// import supportPassive from '@jiubao/passive'
-// var passive = supportPassive()
-// var defaultEventOptions = passive ? {capture: false, passive: true} : false
-
-// export const on = (element, evt, handler, options = defaultEventOptions) => {
-//   element.addEventListener(evt, handler, options)
-//   return () => off(element, evt, handler, options)
-// }
-//
-// export const off = (element, evt, handler, options = defaultEventOptions) => {
-//   element.removeEventListener(evt, handler, options)
-// }
-//
-// export const isFunction = value => {
-//   return typeof value === 'function'
-// }
-//
-// export const inViewport = item => {
-//   var rect = item.getBoundingClientRect()
-//   return (rect.top < window.innerHeight && rect.bottom > 0) &&
-//     (rect.left < window.innerWidth && rect.right > 0)
-// }
 
 export const easing = {
   'cubic': k => --k * k * k + 1,
